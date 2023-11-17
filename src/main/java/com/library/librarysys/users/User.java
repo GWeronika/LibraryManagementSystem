18dc568d9bcb81e38f111ffbb7faa1c9b3b0388d
weronika <weronika.gawel@onet.eu>
2023-11-17 16:54:02 +0100

    template for the User class

---
+++ src/main/java/com/library/librarysys/users/User.java
@@ -0,0 +1,14 @@
+package com.library.librarysys.users;
+
+//guest support will be here soon
+public class User {
+    protected String createAccount() {
+        return "No implementation";
+    }
+    protected String searchForLibraryBranch() {
+        return "No implementation";
+    }
+    protected String browseResources() {
+        return "No implementation";
+    }
+}
