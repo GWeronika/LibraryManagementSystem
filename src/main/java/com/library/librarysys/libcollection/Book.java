a37bffca45724246f223762bbf14c62197b09d26
weronika <weronika.gawel@onet.eu>
2023-11-19 20:36:31 +0100

    Book mistake

--- src/main/java/com/library/librarysys/libcollection/Book.java
+++ src/main/java/com/library/librarysys/libcollection/Book.java
@@ -11,12 +11,10 @@ public class Book {
     private final int bookID;
     private final String title;
     private final String author;
-    private final String releaseYear;
 
-    public Book(int bookID, String title, String author, String releaseYear) {
+    public Book(int bookID, String title, String author) {
         this.bookID = bookID;
         this.title = title;
         this.author = author;
-        this.releaseYear = releaseYear;
     }
 }
