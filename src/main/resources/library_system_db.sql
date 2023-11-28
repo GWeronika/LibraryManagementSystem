a7502c7e8bdfb5b9753ad4dc06001e81930a5a3e
weronika <weronika.gawel@onet.eu>
2023-11-28 20:41:04 +0100

    libraries and openings added to the db

--- src/main/resources/library_system_db.sql
+++ src/main/resources/library_system_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 28, 2023 at 07:27 PM
+-- Generation Time: Lis 28, 2023 at 08:39 PM
 -- Wersja serwera: 10.4.28-MariaDB
 -- Wersja PHP: 8.2.4
 
@@ -54,7 +54,7 @@ CREATE TABLE `book` (
 CREATE TABLE `copy` (
   `copy_id` int(11) NOT NULL,
   `publisher` varchar(255) NOT NULL,
-  `isbn` int(11) NOT NULL,
+  `isbn` varchar(13) NOT NULL,
   `release_year` int(11) NOT NULL,
   `format` enum('BOOK','EBOOK') NOT NULL,
   `language` varchar(255) NOT NULL,
@@ -75,7 +75,7 @@ CREATE TABLE `employee` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `phone_number` int(11) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
   `position` enum('MANAGER','LIBRARIAN') NOT NULL,
   `library_id` int(11) DEFAULT NULL,
   `account_id` int(11) DEFAULT NULL
@@ -89,10 +89,22 @@ CREATE TABLE `employee` (
 
 CREATE TABLE `library` (
   `library_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
-  `phone_number` int(11) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `library`
+--
+
+INSERT INTO `library` (`library_id`, `name`, `location`, `phone_number`, `email`) VALUES
+(1, 'Filia Wierzba', 'Adres Wierzba', '+4812223353', 'wierzba@example.com'),
+(2, 'Filia Dąb', 'Adres Dąb', '+4845556606', 'dab@example.com'),
+(3, 'Filia Sosna', 'Adres Sosna', '+4874888999', 'sosna@example.com'),
+(4, 'Filia Jesion', 'Adres Jesion', '+4834567869', 'jesion@example.com'),
+(5, 'Filia Topola', 'Adres Topola', '+4876543215', 'topola@example.com');
 
 -- --------------------------------------------------------
 
@@ -105,6 +117,40 @@ CREATE TABLE `library_opening` (
   `library_id` int(11) DEFAULT NULL,
   `opening_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `library_opening`
+--
+
+INSERT INTO `library_opening` (`library_opening_id`, `library_id`, `opening_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5),
+(6, 2, 2),
+(7, 2, 5),
+(8, 2, 6),
+(9, 2, 7),
+(10, 2, 1),
+(11, 3, 3),
+(12, 3, 4),
+(13, 3, 5),
+(14, 3, 6),
+(15, 3, 7),
+(16, 3, 1),
+(17, 3, 2),
+(18, 4, 4),
+(19, 4, 5),
+(20, 4, 6),
+(21, 4, 1),
+(22, 4, 2),
+(23, 4, 3),
+(24, 5, 5),
+(25, 5, 7),
+(26, 5, 1),
+(27, 5, 2),
+(28, 5, 4);
 
 -- --------------------------------------------------------
 
@@ -135,13 +181,42 @@ CREATE TABLE `opening` (
   `close_hour` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `opening`
+--
+
+INSERT INTO `opening` (`opening_id`, `day`, `open_hour`, `close_hour`) VALUES
+(1, 'MONDAY', '08:00:00', '16:00:00'),
+(2, 'TUESDAY', '08:00:00', '16:00:00'),
+(3, 'WEDNESDAY', '08:00:00', '16:00:00'),
+(4, 'THURSDAY', '08:00:00', '16:00:00'),
+(5, 'FRIDAY', '08:00:00', '16:00:00'),
+(6, 'SATURDAY', '08:00:00', '16:00:00'),
+(7, 'SUNDAY', '08:00:00', '16:00:00'),
+(8, 'MONDAY', '10:00:00', '18:00:00'),
+(9, 'TUESDAY', '10:00:00', '18:00:00'),
+(10, 'WEDNESDAY', '10:00:00', '18:00:00'),
+(11, 'THURSDAY', '10:00:00', '18:00:00'),
+(12, 'FRIDAY', '10:00:00', '18:00:00'),
+(13, 'SATURDAY', '10:00:00', '18:00:00'),
+(14, 'SUNDAY', '10:00:00', '18:00:00'),
+(15, 'MONDAY', '12:00:00', '20:00:00'),
+(16, 'TUESDAY', '12:00:00', '20:00:00'),
+(17, 'WEDNESDAY', '12:00:00', '20:00:00'),
+(18, 'THURSDAY', '12:00:00', '20:00:00'),
+(19, 'FRIDAY', '12:00:00', '20:00:00'),
+(20, 'SATURDAY', '12:00:00', '20:00:00'),
+(21, 'SUNDAY', '12:00:00', '20:00:00'),
+(22, 'SATURDAY', '10:00:00', '14:00:00'),
+(23, 'SUNDAY', '10:00:00', '14:00:00');
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `order_table`
+-- Struktura tabeli dla tabeli `orders`
 --
 
-CREATE TABLE `order_table` (
+CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `order_date` date NOT NULL,
   `status` enum('REMAINING','READY','CANCELLED') NOT NULL,
@@ -161,7 +236,7 @@ CREATE TABLE `reader` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
-  `phone_number` int(11) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
   `library_card_number` int(11) NOT NULL,
   `account_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -205,6 +280,7 @@ ALTER TABLE `employee`
 --
 ALTER TABLE `library`
   ADD PRIMARY KEY (`library_id`),
+  ADD UNIQUE KEY `name` (`name`),
   ADD UNIQUE KEY `location` (`location`),
   ADD UNIQUE KEY `email` (`email`);
 
@@ -232,9 +308,9 @@ ALTER TABLE `opening`
   ADD PRIMARY KEY (`opening_id`);
 
 --
--- Indeksy dla tabeli `order_table`
+-- Indeksy dla tabeli `orders`
 --
-ALTER TABLE `order_table`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`),
   ADD KEY `reader_id` (`reader_id`),
   ADD KEY `copy_id` (`copy_id`),
@@ -248,6 +324,70 @@ ALTER TABLE `reader`
   ADD UNIQUE KEY `phone_number` (`phone_number`),
   ADD UNIQUE KEY `library_card_number` (`library_card_number`),
   ADD KEY `account_id` (`account_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `copy`
+--
+ALTER TABLE `copy`
+  MODIFY `copy_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `library`
+--
+ALTER TABLE `library`
+  MODIFY `library_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `library_opening`
+--
+ALTER TABLE `library_opening`
+  MODIFY `library_opening_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `loan`
+--
+ALTER TABLE `loan`
+  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `opening`
+--
+ALTER TABLE `opening`
+  MODIFY `opening_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reader`
+--
+ALTER TABLE `reader`
+  MODIFY `reader_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -283,12 +423,12 @@ ALTER TABLE `loan`
   ADD CONSTRAINT `loan_ibfk_3` FOREIGN KEY (`reader_id`) REFERENCES `reader` (`reader_id`);
 
 --
--- Constraints for table `order_table`
+-- Constraints for table `orders`
 --
-ALTER TABLE `order_table`
-  ADD CONSTRAINT `order_table_ibfk_1` FOREIGN KEY (`reader_id`) REFERENCES `reader` (`reader_id`),
-  ADD CONSTRAINT `order_table_ibfk_2` FOREIGN KEY (`copy_id`) REFERENCES `copy` (`copy_id`),
-  ADD CONSTRAINT `order_table_ibfk_3` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`);
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`reader_id`) REFERENCES `reader` (`reader_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`copy_id`) REFERENCES `copy` (`copy_id`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`);
 
 --
 -- Constraints for table `reader`
